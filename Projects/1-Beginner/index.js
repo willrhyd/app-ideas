c0dd4943dc84023fdaa75dad2f5d009150cad0d3
willrhyd <34925929+willrhyd@users.noreply.github.com>
2020-02-10 17:54:08 +0000

    Delete index.js

--- Projects/1-Beginner/index.js
+++
@@ -1,6 +0,0 @@
-$( "input" ).change(function(){
-  var radVal = $("input").val();
-  $( ".box" ).css("border-radius", radVal + "px");
-
-  $("#outputCSS").text("border-radius: " + radVal + "px" );
-});
